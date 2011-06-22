6bbf50b17fdda7739fc7217800d1a63e81497304
Imad Mouaddine <imad@ecomstrategy.ca>
2011-06-22 15:40:58 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0.rc4'
+gem 'sqlite3'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+gem "devise", ">= 1.3.4"
+gem "rails-footnotes", ">= 3.7", :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylejonction::Application.config.secret_token = 'fad9c626642e97375601548a22137aca078b44476ef0107dadedb010a376bdf3bbb0c873e43fb28af90cddb1b4e6adbad1df53e8985ba1c1d75ce86e2a2e5756'
