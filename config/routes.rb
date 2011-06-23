ecf7b4e7b5c202950d4cf64c5a45f25044518290
Imad Mouaddine <imad@ecomstrategy.ca>
2011-06-22 23:20:11 -0400

    S

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Stylejonction::Application.routes.draw do
+ 
+
   #get \"users\/show\"
 
   root :to => "home#index"
 
   devise_for :users
-  resources :users, :only => :show
+  resources :users, :only => :show 
 
 
   # The priority is based upon order of creation:
