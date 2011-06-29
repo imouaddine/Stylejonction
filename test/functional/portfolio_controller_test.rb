a9a8042ef37f39c0fc77822e6ccb0489e9bde492
Imad Mouaddine <imad@ecomstrategy.ca>
2011-06-29 08:19:54 -0400

    Remove home controller test. not used anymore

--- test/functional/portfolio_controller_test.rb
+++ test/functional/portfolio_controller_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class PortfolioControllerTest < ActionController::TestCase
   
+  include Devise::TestHelpers
+  
   fixtures :users, :portfolios
   
+  def setup
+    
+    sign_in @user
+    @request.host = "#{@user.subdomain}.example.com"
+  end
  
   
   
   test "should get show" do
+   
+    
+    
     get :show
     assert_response :success
   end
