799b81af0fc2eca21931d04cccc25e8fdee3d18f
Ignacy Moryc <imoryc@gmail.com>
2011-06-30 12:46:25 +0200

    Added new action to create a portfolio if it doesn't exist

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,8 +1,32 @@
 class PortfolioController < ApplicationController
-  before_filter :get_portfolio
+  before_filter :get_portfolio, :except => [:new, :create]
   before_filter :authenticate_user!, :except => [:show]
 
+  def new
+    if !current_user.portfolio.nil? 
+      redirect_to(portfolio_path(@user.portfolio), :notice => "You already, have a portfolio")
+    else
+      @portfolio = Portfolio.new
+    end
+  end
+
+  def create
+    @portfolio = Portfolio.new(params[:portfolio])
+    current_user.portfolio = @portfolio
+    
+    if @portfolio.save
+      current_user.save
+      redirect_to portfolio_path(@portfolio)
+    else
+      render :action => "new"
+    end
+  end
+
   def show
+    if params[:id].present?
+      @portfolio = @user.portfolio
+    end
+
     respond_to do |format|
       format.html 
       format.json { render json: @portfolio }
@@ -13,15 +37,15 @@ class PortfolioController < ApplicationController
   def edit_font; end
 
   def update
-      respond_to do |format|
-        if @portfolio.update_attributes(params[:portfolio])
-          format.html { redirect_to portfolio_path @portfolio, notice: 'Portfolio was successfully updated.' }
-          format.json { head :ok }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @portfolio.update_attributes(params[:portfolio])
+        format.html { redirect_to portfolio_path @portfolio, notice: 'Portfolio was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
+    end
   end
-  
+
 end
