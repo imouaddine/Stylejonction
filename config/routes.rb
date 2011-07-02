ee77339e34eb0b360a59c6ccb0eeff23fa57b476
Ignacy Moryc <imoryc@gmail.com>
2011-07-02 17:43:57 +0200

    portfolio/:id routes to '/'

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Stylejonction::Application.routes.draw do
  
  match "/users" => "users#index"
  match '#/:page' => 'pages#:page', :as => :page
+ match "portfolio/:id" => "pages#index"
   
  constraints(Subdomain) do
     match '/' => 'portfolio#show'
@@ -23,53 +24,4 @@ Stylejonction::Application.routes.draw do
  end
   
  root :to => "pages#index"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-
 end
