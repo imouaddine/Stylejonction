d924c210403f9de1b47a1fcd9226ee803ae4c10f
Ignacy Moryc <imoryc@gmail.com>
2011-07-18 08:02:33 +0200

    Fixed some tests; some of those borken by the preview screen are still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Stylejonction::Application.routes.draw do
- require 'subdomain'
+  require 'subdomain'
 
- devise_for :users
+  devise_for :users
 
   match "/users" => "users#index"
 
- constraints(Subdomain) do
+  constraints(Subdomain) do
     match '/' => 'portfolios#edit_layout'
-    
+
     resource :portfolio, :except => [:destroy, :new] do
       member do
         get 'edit_layout'
@@ -17,29 +17,31 @@ Stylejonction::Application.routes.draw do
       end
       resources :projects  do
         member do
-         post 'invite'
+          post 'invite'
         end
       end
-      
-      
-   end
-  end
-  
-  resources :invitations, :only => [:show]
-  resources :custom_backgrounds, :only => [:edit, :update, :create] 
-  resources :predefined_backgrounds, :only => [:show]
-  
-  resources :fonts, :only => [:show]
-  resources :images do
-    member do 
-       post 'upload'
-       match 'crop'
-       match 'scale_to_fit'
+
+
     end
   end
-  
 
- #match "portfolio/:id" => "portfolios#show"
- match '#/:page' => 'pages#:page', :as => :page
- root :to => "pages#index"
+  resources :invitations, :only => [:show]
+  resources :custom_backgrounds, :only => [:edit, :update, :create]
+  resources :predefined_backgrounds, :only => [:show]
+
+  resources :fonts, :only => [:show]
+  resources :images do
+    member do
+      post 'upload'
+      match 'crop'
+      match 'scale_to_fit'
+    end
+  end
+
+  # This duplicates upload_image but doesn't need id
+  match "/images/upload" => 'images#upload', :via => "post"
+
+  #match "portfolio/:id" => "portfolios#show"
+  match '#/:page' => 'pages#:page', :as => :page
+  root :to => "pages#index"
 end
