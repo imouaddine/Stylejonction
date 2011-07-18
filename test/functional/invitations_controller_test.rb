9d97a46d046245428a92852e85f6d7562dc79491
Ignacy Moryc <imoryc@gmail.com>
2011-07-18 08:18:35 +0200

    Public view for portfolios

--- test/functional/invitations_controller_test.rb
+++ test/functional/invitations_controller_test.rb
@@ -3,13 +3,12 @@ require 'test_helper'
 class InvitationsControllerTest < ActionController::TestCase
   def setup
     @user = Factory.create(:user)
-    @invited_user = Factory.create(:user, :email => "ignacy@email.com")
     @project = Factory.create(:project, :portfolio => @user.portfolio)
   end
 
   test "invitation gets created and routing works" do
-    @project.invite(@invited_user.email)
-    sign_in @invited_user
+    @project.invite("ignacy@email.com")
+
     get :show, :id => @project.invitations.first
     @project.reload
     assert @project.invitations.first.used?, "Should be used"
